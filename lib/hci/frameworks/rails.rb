4d90b59179e8de4477bcd0996a77e85c84da302b
Sam Oliver <sam@samoliver.com>
2011-03-20 11:41:14 +0000

    Add Rails 3 compatible initializer

--- lib/hci/frameworks/rails.rb
+++ lib/hci/frameworks/rails.rb
@@ -1,4 +1,13 @@
-Hci::Client.load_path = Rails.root + Hci::Client.load_path
-Rails.configuration.after_initialize do
-  Hci::Client.load!
+if defined?(ActionController::Metal)
+  class Railtie < Rails::Railtie
+    initializer :load_hci do |app|
+      Hci::Client.load_path = Rails.root + Hci::Client.load_path
+      Hci::Client.load!
+    end
+  end
+else
+  Rails.configuration.after_initialize do
+    Hci::Client.load_path = Rails.root + Hci::Client.load_path
+    Hci::Client.load!
+  end
 end
