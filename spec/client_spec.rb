1a2fb114977996564d007ba210dc58f55037623c
Sam Oliver <sam@samoliver.com>
2012-02-15 09:44:33 +0000

    Remove spec for loading task definitions

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,13 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Client" do
-  it "should load tasks" do
-    SharedWorkforce::Client.load_path = File.dirname(__FILE__) + '/tasks'
-    SharedWorkforce::Client.load!
-    
-    SharedWorkforce::Task.find("Approve photo").should_not == nil
-  end
-  
   it "should return the current version number" do
     SharedWorkforce::Client.version.should == SharedWorkforce::VERSION
   end
