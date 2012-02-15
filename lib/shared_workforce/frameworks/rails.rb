ced5b01f37e06573859eb159aebf00017a441582
Sam Oliver <sam@samoliver.com>
2012-02-15 16:58:07 +0000

    Remove SW initialization and load path addition from initialiser

--- lib/shared_workforce/frameworks/rails.rb
+++ lib/shared_workforce/frameworks/rails.rb
@@ -2,13 +2,6 @@ if defined?(ActionController::Metal)
   class Railtie < Rails::Railtie
     initializer 'shared_workforce' do |app|      
       app.config.middleware.use SharedWorkforce::EndPoint
-      SharedWorkforce::Client.load_path = Rails.root + SharedWorkforce.configuration.load_path
-      SharedWorkforce::Client.load!
     end
-  end
-else
-  Rails.configuration.after_initialize do
-    SharedWorkforce::Client.load_path = Rails.root + SharedWorkforce.configuration.load_path
-    SharedWorkforce::Client.load!
   end
 end
