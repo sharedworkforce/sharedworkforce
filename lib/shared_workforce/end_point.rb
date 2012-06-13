3f0a44e8fb854cbbdad02def301174370d09e541
Sam Oliver <sam@samoliver.com>
2012-06-13 09:39:03 +0100

    Remove legacy callback path compatibility

--- lib/shared_workforce/end_point.rb
+++ lib/shared_workforce/end_point.rb
@@ -6,7 +6,7 @@ module SharedWorkforce
     end
 
     def call(env)
-      if env["PATH_INFO"] =~ %r{^/#{callback_path}} || env["PATH_INFO"] =~ %r{^/hci_task_result} #legacy compatibility, can be removed in next release
+      if env["PATH_INFO"] =~ %r{^/#{callback_path}}
         process_response(Rack::Request.new(env).body.read)
       else
         @app.call(env) if @app.respond_to? :call
