d85e87292e28499c53580297d6190c2898e97f1d
Sam Oliver <sam@samoliver.com>
2012-08-01 11:34:31 +0100

    Start poller in after_initialize block (fixes #15)

--- lib/shared_workforce/frameworks/rails.rb
+++ lib/shared_workforce/frameworks/rails.rb
@@ -2,13 +2,15 @@ if defined?(ActionController::Metal)
   class Railtie < Rails::Railtie
     initializer 'shared_workforce' do |app|      
       app.config.middleware.use SharedWorkforce::EndPoint
-      if Rails.env.development?
-        # Stop log buffering when using Foreman in development
-        $stdout.sync = true
-      	if SharedWorkforce.configuration.valid?
-          SharedWorkforce::ResponsePoller.start
-        else
-          puts 'Shared Workforce: API key not configured.'
+      app.config.after_initialize do
+        if Rails.env.development?
+        	if SharedWorkforce.configuration.valid?
+            # Stop log buffering when using Foreman in development
+            $stdout.sync = true
+            SharedWorkforce::ResponsePoller.start
+          else
+            puts 'Shared Workforce: API key not configured.'
+          end
         end
       end
     end
